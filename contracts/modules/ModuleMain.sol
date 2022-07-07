af8222f8dde7f20d09b1f831dcac8f91d3b882a2
zz <517669936@qq.com>
2022-07-07 15:58:35 +0800

    [ModuleMain]: Add ModuleMain

--- contracts/modules/ModuleMain.sol
+++ contracts/modules/ModuleMain.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
-import "./commons/ModuleAuth.sol";
+import "../modules/commons/ModuleCall.sol";
+import "../modules/commons/ModuleAuth.sol";
 
-// contract ModuleMain is ModuleAuth {
-//     constructor(address _factory) ModuleAuth(_factory) {}
-// }
+contract ModuleMain is ModuleCall, ModuleAuth {
+    constructor(address _factory) ModuleAuth(_factory) {}
+
+    // FIXME: Removing If Adding ModuleHook
+    receive() external payable {}
+}
