009f1ee62a9505bd8e46cbc588502f14745db9b0
zz <517669936@qq.com>
2023-01-29 10:12:55 +0800

    fix: #8 add comment to liboptim call

--- contracts/utils/LibOptim.sol
+++ contracts/utils/LibOptim.sol
@@ -20,6 +20,9 @@ library LibOptim {
         bytes calldata _data
     ) internal returns (bool r) {
         assembly {
+            // We will not restore a new point address to `0x40`, as
+            // we will not reuse the data.
+            // Notice that there may be dirty data in the free memory.
             let tmp := mload(0x40)
             calldatacopy(tmp, _data.offset, _data.length)
 
